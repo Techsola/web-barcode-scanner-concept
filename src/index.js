bc3206eac942280aa0dddb774f59ba21bed8f983
jnm2 <me@jnm2.com>
2022-09-28 10:44:21 -0400

    BarcodeDetection experimental API works super well

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import Quagga from '@ericblade/quagga2';
 
 (async function() {
-    const previewContainer = document.getElementById('preview');
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: { facingMode: 'environment', width: 3480, height: 2160 },
+    });
 
-    await Quagga.init({
-        inputStream: {
-            type: "LiveStream",
-            constraints: { facingMode: 'environment', width: 1280 },
-            target: previewContainer,
-            area: { top: "40%", bottom: "40%" },
-        },
-        decoder: {
-            readers: ['upc_reader', 'upc_e_reader', 'code_128_reader', 'code_39_reader', 'code_93_reader'],
-        },
-        numOfWorkers: navigator.hardwareConcurrency,
+    const previewElement = document.getElementById('preview');
+    previewElement.srcObject = stream;
+    await previewElement.play();
+
+    const barcodeDetector = new BarcodeDetector({
+        formats: ['code_128', 'code_39', 'code_93', 'upc_a', 'upc_e'],
     });
 
     const resultOverlayElement = document.getElementById('result-overlay');
@@ -25,21 +23,43 @@ import Quagga from '@ericblade/quagga2';
         resultOverlayElement.style.display = 'none';
     };
 
-    Quagga.onDetected(function(result) {
-        const resultText = `${result.codeResult.format} "<b>${result.codeResult.code}</b>"`;
-        results.set(resultText, (results.get(resultText) || 0) + 1);
+    while (true) {
+        const barcodes = await barcodeDetector.detect(previewElement);
+        const barcode = findNearestToCenter(barcodes);
 
-        resultDisplayElement.innerHTML = [...results.entries()]
-            .sort((a, b) => b[1] - a[1])
-            .splice(0, 3)
-            .map(entry => `${entry[0]} (${entry[1]} reads)`)
-            .join('<br>');
+        if (barcode != null) {
+            const resultText = `${barcode.format} "<b>${barcode.rawValue}</b>"`;
+            results.set(resultText, (results.get(resultText) || 0) + 1);
 
-        if (resultOverlayElement.style.display == 'none') {
-            resultOverlayElement.style.display = null;
-            if ('vibrate' in navigator) navigator.vibrate(50);
+            resultDisplayElement.innerHTML = [...results.entries()]
+                .sort((a, b) => b[1] - a[1])
+                .splice(0, 3)
+                .map(entry => `${entry[0]} (${entry[1]} reads)`)
+                .join('<br>');
+
+            if (resultOverlayElement.style.display == 'none') {
+                resultOverlayElement.style.display = null;
+                if ('vibrate' in navigator) navigator.vibrate(50);
+            }
         }
-    });
+    }
 
-    Quagga.start();
+    function findNearestToCenter(barcodes) {
+        let nearest = null;
+        let nearestDistance = 0;
+
+        const videoCenter = previewElement.videoHeight / 2;
+
+        for (const barcode of barcodes) {
+            const barcodeCenter = barcode.boundingBox.y + barcode.boundingBox.height / 2;
+            const distance = Math.abs(videoCenter - barcodeCenter);
+
+            if (nearest == null || distance < nearestDistance) {
+                nearest = barcode;
+                nearestDistance = distance;
+            }
+        }
+
+        return nearest;
+    }
 })();
