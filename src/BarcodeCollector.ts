99ccbeb10981a22f38b3a3868c9775a2f83ee540
jnm2 <me@jnm2.com>
2022-10-24 19:28:35 -0400

    Decouple starting collection mode from result count

--- src/BarcodeCollector.ts
+++ src/BarcodeCollector.ts
@@ -1,3 +1,5 @@
+const enum BarcodeCollectorMode { Waiting, Collecting, ShowingResult }
+
 export default class BarcodeCollector {
     private readonly results = new Array<{
         readonly format: string,
@@ -5,7 +7,7 @@ export default class BarcodeCollector {
         readCount: number,
     }>();
 
-    private showingResults = false;
+    private mode = BarcodeCollectorMode.Waiting;
 
     constructor(
         private readonly showCollectionStarting: (duration: number) => void,
@@ -14,27 +16,29 @@ export default class BarcodeCollector {
     }
 
     onDetected(format: string, code: string) {
-        if (code.length == 0 || this.showingResults) return;
-
-        if (this.results.length == 0) {
-            const duration = 700;
-            this.showCollectionStarting(duration);
-            setTimeout(() => this.onCollectionFinished(), duration);
-        }
+        if (code.length == 0 || this.mode == BarcodeCollectorMode.ShowingResult) return;
 
         let entry = this.results.find(r => r.format == format && r.code == code);
         if (!entry) this.results.push(entry = { format, code, readCount: 0 });
         entry.readCount++;
+
+        if (this.mode == BarcodeCollectorMode.Waiting) {
+            this.mode = BarcodeCollectorMode.Collecting;
+
+            const duration = 700;
+            this.showCollectionStarting(duration);
+            setTimeout(() => this.onCollectionFinished(), duration);
+        }
     }
 
     private onCollectionFinished() {
-        this.showingResults = true;
+        this.mode = BarcodeCollectorMode.ShowingResult;
         const bestEntry = this.results.sort((a, b) => b.readCount - a.readCount)[0];
         this.showResults(bestEntry.format, bestEntry.code);
     }
 
     reset() {
-        this.showingResults = false;
+        this.mode = BarcodeCollectorMode.Waiting;
         this.results.length = 0;
         this.hideResults();
     }
