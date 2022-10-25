54f908644cd76e67384aae7d3264a84a30ed6f9a
jnm2 <me@jnm2.com>
2022-10-24 22:09:56 -0400

    Fix scanning in landscape

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,6 @@ import BarcodeCollector from './BarcodeCollector';
 
 (async function() {
     const previewContainer = document.getElementById('preview')!;
-
-    await Quagga.init({
-        inputStream: {
-            type: "LiveStream",
-            constraints: { facingMode: 'environment', width: 1280 },
-            target: previewContainer,
-            area: { top: "40%", bottom: "40%" },
-        },
-        decoder: {
-            readers: ['upc_reader', 'upc_e_reader', 'code_128_reader', 'code_39_reader', 'code_93_reader'],
-        },
-        numOfWorkers: navigator.hardwareConcurrency,
-    });
-
     const collectionProgressAnimation = document.getElementById('collection-progress') as unknown as SVGAnimationElement;
     const resultOverlayElement = document.getElementById('result-overlay')!;
     const resultDisplayElement = document.getElementById('result-display')!;
@@ -45,5 +31,35 @@ import BarcodeCollector from './BarcodeCollector';
             barcodeCollector.onDetected(result.codeResult.format, result.codeResult.code);
     });
 
-    Quagga.start();
+    await initializeAndStartQuagga();
+
+    if ('orientation' in screen) {
+        screen.orientation.addEventListener('change', async () => {
+            Quagga.stop();
+            await initializeAndStartQuagga();
+        }, false);
+    };
+
+    async function initializeAndStartQuagga() {
+        const isLandscape = 'orientation' in screen
+            ? screen.orientation.type == 'landscape-primary' || screen.orientation.type == 'landscape-secondary'
+            : undefined;
+
+        await Quagga.init({
+            inputStream: {
+                type: "LiveStream",
+                constraints: { facingMode: 'environment', width: 1280 },
+                target: previewContainer,
+                area: isLandscape
+                    ? { left: "30%", top: "40%", right: "30%", bottom: "40%" }
+                    : { top: "40%", bottom: "40%" },
+            },
+            decoder: {
+                readers: ['upc_reader', 'upc_e_reader', 'code_128_reader', 'code_39_reader', 'code_93_reader'],
+            },
+            numOfWorkers: navigator.hardwareConcurrency,
+        });
+
+        Quagga.start();
+    }
 })();
